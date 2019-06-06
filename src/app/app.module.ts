ee7a857064106f3ffb4f00e9a4c7c87fbb847b32
gc <dummy@w5rsfz.com>
2019-06-06 21:43:09 +0200

    feat: barcode plugin invocation and basic UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	entryComponents: [],
+	imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+	providers: [StatusBar, SplashScreen, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, BarcodeScanner],
+	bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+	constructor(private barcodeScanner: BarcodeScanner) {}
 
-  constructor() {}
+	scanResult: any;
 
+	openBarCodeScanner() {
+		this.barcodeScanner
+			.scan()
+			.then(barcodeData => {
+				console.log('Barcode data', barcodeData);
+				this.scanResult = barcodeData;
+			})
+			.catch(err => {
+				console.log('Error', err);
+			});
+	}
 }
